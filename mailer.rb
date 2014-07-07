ecce43e922a127bab5fe8d5567521c3b8c667b51
Kevin <gaohongwei@gmail.com>
2014-07-07 09:12:38 -0700

    Update mailer.rb

--- mailer.rb
+++ mailer.rb
@@ -2,7 +2,7 @@ export EMAIL=me@gmail.com
 export PASSWORD=mypwd
 
 view ./config/application.rb
-
+config.action_mailer.default_url_options = { :host => 'five9.com' }
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
@@ -12,3 +12,12 @@ ActionMailer::Base.smtp_settings = {
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
+
+Or set up host by the following way:
+class ApplicationController < ActionController::Base
+	before_filter :mailer_set_url_options
+
+	def mailer_set_url_options
+	  ActionMailer::Base.default_url_options[:host] = request.host_with_port
+	end
+end
