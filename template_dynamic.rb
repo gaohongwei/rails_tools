3cdbcf182eea429957eea0dc7e18763d22efcb7a
Kevin <gaohongwei@gmail.com>
2014-07-16 21:28:23 -0700

    Update template_dynamic.rb

--- template_dynamic.rb
+++ template_dynamic.rb
@@ -1,7 +1,10 @@
 @ar=[ {name:'gwei',pay:20}, {name:'jeff',pay:30}, {name:'just',pay:40} ]
 
 tpl="<% @ar.each do |e| %> name:<%=e[:name]%>, pay:<%=e[:pay]%> <% end %>"
-
 render :inline =>tpl,:layout => "application"
+
+template = ERB.new(tpl)
+@ss=template.result(binding)
+
 
 data = render_to_string( :action => :show,:layout => false)
