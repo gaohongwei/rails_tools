e34396b1118a6bbdd33fa6a5b034472c8614f94d
Kevin <gaohongwei@gmail.com>
2014-07-30 08:21:36 -0700

    Update rvm.rb

--- rvm.rb
+++ rvm.rb
@@ -10,3 +10,13 @@ rvm list known
 rvm install 2.1.1
 rvm use ruby-2.1.1
 rvm remove ruby-2.1.1
+
+rvmrc
+There are three different kinds of rvmrc files:
+System wide configuration: /etc/rvmrc
+User wide configuration: ~/.rvmrc
+Project wide configuration:  .rvmrc
+
+The most interesting one is the project .rvmrc. 
+Every time you cd, RVM looks for a file called .rvmrc. 
+If it finds it, it executes it.
