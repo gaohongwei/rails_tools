4fd39e92652c97dcb52ff5c557e23f9cb64284ab
Kevin <gaohongwei@gmail.com>
2014-08-03 11:13:32 -0700

    Update association.rb

--- association.rb
+++ association.rb
@@ -1,5 +1,5 @@
 
-:foreign_key
+:foreign_key, foreign_key, exist in child table
 has_many/has_one/belongs_to
 By convention, Rails assumes that the column used to hold the foreign key on the other model is the name of this model with the suffix _id added. The :foreign_key option lets you set the name of the foreign key directly:
 
@@ -12,11 +12,23 @@ By convention, Rails assumes that the column used to hold the primary key of the
 has_many/has_one
 The :source option specifies the source association name for a 
 has_many/has_one :through association. 
-You only need to use this option if the name of the source association cannot be automatically inferred from the association name.
+You only need to use this option if the name of the source association cannot be 
+automatically inferred from the association name.
 
 :class_name
 has_many/has_one/belongs_to
-If the name of the other model cannot be derived from the association name, you can use the :class_name option to supply the model name.
+If the name of the other model cannot be derived from the association name, you can 
+use the :class_name option to supply the model name.
+
+---------------------
+:source and :class_name are conceptually the same, just need to be different for different uses.
+
+:source is used (optionally) to define the associated model name when you're using has_many  through; 
+:class_name is used (optionally) in a simple has many relationship. 
+
+Both are needed only if Rails cannot figure out the class name on its own. 
+
+
 
 Self Joins
 class Employee < ActiveRecord::Base
