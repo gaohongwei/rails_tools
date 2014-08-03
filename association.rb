61343e95145e7387dcccc53d9a395e83d2c64132
Kevin <gaohongwei@gmail.com>
2014-08-02 17:04:48 -0700

    Create association.rb

---
+++ association.rb
@@ -0,0 +1,30 @@
+
+:foreign_key
+has_many/has_one/belongs_to
+By convention, Rails assumes that the column used to hold the foreign key on the other model is the name of this model with the suffix _id added. The :foreign_key option lets you set the name of the foreign key directly:
+
+:primary_key
+By convention, Rails assumes that the column used to hold the primary key of the association is id. You can override this and explicitly specify the primary key with the :primary_key option.
+
+
+
+:source
+has_many/has_one
+The :source option specifies the source association name for a 
+has_many/has_one :through association. 
+You only need to use this option if the name of the source association cannot be automatically inferred from the association name.
+
+:class_name
+has_many/has_one/belongs_to
+If the name of the other model cannot be derived from the association name, you can use the :class_name option to supply the model name.
+
+Self Joins
+class Employee < ActiveRecord::Base
+  has_many :subordinates, class_name: "Employee",
+                          foreign_key: "manager_id"
+ 
+  belongs_to :manager, class_name: "Employee"
+end
+
+
+http://guides.rubyonrails.org/association_basics.html
