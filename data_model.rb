4aeedfe722f823aac3bcfc91bdd21d4dcb1735d2
Kevin <gaohongwei@gmail.com>
2014-09-01 08:11:29 -0700

    Update data_model.rb

--- data_model.rb
+++ data_model.rb
@@ -9,3 +9,22 @@
   scope :bycontroller, ->(controller) {
     where(:controller=>controller)
   } 
+
+Virtual attribute
+  attr_accessor :layout_type,:column_per_row
+  
+Access control
+attr_accessible(*args) public
+Specifies a white list of model attributes that can be set via mass-assignment.
+
+attr_accessible(*args) public
+Specifies a white list of model attributes that can be set via mass-assignment.
+
+Like attr_protected, a role for the attributes is optional, if no role is provided then :default is used. 
+A role can be defined by using the :as option.
+
+This is the opposite of the attr_protected macro: 
+Mass-assignment will only set attributes in this list, to assign to the rest of attributes 
+you can use direct writer methods. This is meant to protect sensitive attributes from 
+being overwritten by malicious users tampering with URLs or forms. 
+If you’d rather start from an all-open default and restrict attributes as needed, have a look at attr_protected.
