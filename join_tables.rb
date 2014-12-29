1749ca316ed5614fcfcd288f793c763a31551f89
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2014-12-28 19:00:56 -0800

    Update join_tables.rb

--- join_tables.rb
+++ join_tables.rb
@@ -9,3 +9,13 @@ end
 
 Post.joins(:tags).group("tags.name").count
 # => {"tag1" => 4, "tag2" => 2, "tag3" => 5}
+
+class OptionUser < ActiveRecord::Base
+  #OptionUser.joins(:option)
+  belongs_to :option, :class_name=>'TaskOption',:foreign_key=>'option_id'
+  OptionUser.joins(:option=>(:task))
+
+  scope :by_task,lambda {|id|
+    joins(:option=>(:task)).merge(Task.where(id:id))
+  } # id can be a array, [1,2]
+end
