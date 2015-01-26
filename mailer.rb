1fa9293495da74b1ff6e3b3bcc1b9dac5d1573df
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2015-01-26 09:51:13 -0800

    Update mailer.rb

--- mailer.rb
+++ mailer.rb
@@ -17,10 +17,20 @@ Or set up host/default_url_options and gmail account/password by the following w
 class ApplicationController < ActionController::Base
 	before_filter :mailer_set_url_options
 
-	def mailer_set_url_options
-	  ActionMailer::Base.default_url_options[:host] = request.host_with_port
-	  ActionMailer::Base.smtp_settings[:user_name] = 'abc@gmail.com'
-	  ActionMailer::Base.smtp_settings[:password] = 'mypwd'	  
-	end
+  def mailer_set_url_options
+    # default_url_options Not working here. 
+    # Have to move to config to work    
+    #config.action_mailer.default_url_options = { host:'xietiao.herokuapp.com'}   
+    #Or  request.host_with_port
+    ActionMailer::Base.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "gmail.com",
+      :user_name            => ENV["EMAIL"],
+      :password             => ENV["PASSWORD"],    
+      :authentication       => "plain",
+      :enable_starttls_auto => true
+    }     
+  end
 end
 
