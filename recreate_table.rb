71f37fce357068c6bf2d8557a24cfb085ca1dea0
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2015-02-24 19:20:53 -0800

    Update recreate_table.rb

--- recreate_table.rb
+++ recreate_table.rb
@@ -1,3 +1,4 @@
 ##################### Drop table
 ActiveRecord::Migration.drop_table(:menus)
 ActiveRecord::Base.connection.execute("TRUNCATE table_name")
+rake db:migrate:redo VERSION=20150221001521
