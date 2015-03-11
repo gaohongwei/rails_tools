71d4e6b45d8fafa191b713b7745e83724bcbe5dc
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2015-03-11 11:33:25 -0700

    Update db_recreate.rb

--- db_recreate.rb
+++ db_recreate.rb
@@ -4,6 +4,6 @@ ActiveRecord::Migration.drop_table(:menus)
 # Truncate and reset ID to 1
 ActiveRecord::Base.connection.execute("TRUNCATE table_name")
 ActiveRecord::Base.connection.execute("TRUNCATE comments;TRUNCATE comments")
-ActiveRecord::Base.connection.reset_pk_sequence!('comments')
+ActiveRecord::Base.connection.reset_pk_sequence!('comments') # only pg needs this
 
 rake db:migrate:redo VERSION=20150221001521
