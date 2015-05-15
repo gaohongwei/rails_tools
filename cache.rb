6d260423461d77e2dcc69f27c76d5b253f3a34ad
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2015-05-15 10:19:37 -0700

    Update cache.rb

--- cache.rb
+++ cache.rb
@@ -21,3 +21,21 @@ rake tmp:sessions:clear #Clear all files in tmp/sessions
 rake tmp:sockets:clear  #Clear all files in tmp/sockets
 rake db:sessions:clear  #Rake can also clear the sessions table if you’re using one:
 
+==================== cache ====================
+# config
+config/environments/production.rb
+config/application.rb
+config.action_controller.perform_caching = true
+
+# Page Caching
+Page caching can’t be applied to situation authentication
+Enable page caching, 
+class ProductsController < ActionController
+  caches_page :index
+
+The page cache directory is set to Rails.public_path (public) by default 
+config.action_controller.page_cache_directory
+It helps avoid naming conflicts to chnage the default from public
+but it will require web server reconfiguration to tell the web server for the cached files.
+
+
