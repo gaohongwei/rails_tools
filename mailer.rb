b241065579addb05cddfd643f3ea57507f5a8ae2
kevin-wei-pan <kwei@paloaltonetworks.com>
2015-10-15 22:14:15 -0700

    Update mailer.rb

--- mailer.rb
+++ mailer.rb
@@ -2,13 +2,17 @@ export EMAIL=me@gmail.com
 export PASSWORD=mypwd
 
 view ./config/application.rb
-config.action_mailer.default_url_options = { :host => 'five9.com' }
-ActionMailer::Base.smtp_settings = {
+config.action_mailer.default_url_options = { :host => 'me.com' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+
+config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "gmail.com",
-  :user_name            => ENV["EMAIL"],
-  :password             => ENV["PASSWORD"],    
+  :user_name            => 'me@gmail.com',
+  :password             => 'pwd',
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
@@ -22,6 +26,7 @@ class ApplicationController < ActionController::Base
     # Have to move to config to work    
     #config.action_mailer.default_url_options = { host:'xietiao.herokuapp.com'}   
     #Or  request.host_with_port
+    
     ActionMailer::Base.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
@@ -30,7 +35,11 @@ class ApplicationController < ActionController::Base
       :password             => ENV["PASSWORD"],    
       :authentication       => "plain",
       :enable_starttls_auto => true
-    }     
+    }
+    ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.perform_deliveries = true
+    ActionMailer::Base.raise_delivery_errors = true
+    ActionMailer::Base.default_url_options = { host:request.host_with_port}    
   end
 end
 
