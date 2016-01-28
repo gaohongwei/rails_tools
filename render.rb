baf9e5f337c18b5f52024bfc7e6782cf99445474
kevin-wei-pan <kwei@paloaltonetworks.com>
2016-01-27 19:54:06 -0800

    Create render.rb

---
+++ render.rb
@@ -0,0 +1,27 @@
+
+
+Render
+To store the result of the render to a string, 
+you could try the render_to_string method of 
+
+Render a partial to a string
+AbstractController::render_to_string
+partial_string = render_to_string(:partial => 'my_partial')
+
+
+Render html/json
+  def create
+    @task = Task.new(params[:task])
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.json { render json: @task, status: :created, location: @task }
+        format.js
+      else
+        format.html { render action: "new" }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+        format.js
+      end
+    end
+  end
