c883609e6e8eee2cbf2bb3feb89bb48c36eba35b
kevin-wei-pan <kwei@paloaltonetworks.com>
2016-01-27 19:57:50 -0800

    Create log.rb

---
+++ log.rb
@@ -0,0 +1,9 @@
+
+ActiveRecord::Base.logger = YouLogger.new
+
+In Rails 4 all 
+replace Rails.logger in an initializer.
+
+exception_logger
+https://github.com/defunkt/exception_logger/tree/master
+
