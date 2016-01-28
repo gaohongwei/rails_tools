16ee9fcf88ab73ff7fff0a5a59c2115f671b97eb
kevin-wei-pan <kwei@paloaltonetworks.com>
2016-01-28 14:19:38 -0800

    Update log.rb

--- log.rb
+++ log.rb
@@ -9,3 +9,17 @@ replace Rails.logger in an initializer.
 exception_logger
 https://github.com/defunkt/exception_logger/tree/master
 
+######
+require 'singleton'
+class Log2db < Logger
+  def format_message(severity, timestamp, progname, msg)
+    "wgh #{timestamp.to_formatted_s(:db)} #{severity} #{msg}\n"
+    Template.create(name:msg,subject:msg,body:msg)
+  end
+end
+
+# initializer
+logfile = File.open("#{Rails.root}/log/custom.log", 'a')  # create log file
+logfile.sync = true  # automatically flushes data to file
+CUSTOM_LOGGER = Log2db.new(logfile)  # constant accessible anywhere
+Rails.logger.extend(ActiveSupport::Logger.broadcast(CUSTOM_LOGGER))
