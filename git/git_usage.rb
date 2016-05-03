b5fc5f54aceef1b06875e5f5d9b0f87fbd4ac159
gaohong wei <gaohongwei@gmail.com>
2016-05-03 15:38:18 -0700

    add git usage

---
+++ git/git_usage.rb
@@ -0,0 +1,18 @@
+### .git/config
+[core]
+        repositoryformatversion = 0
+        filemode = true
+        bare = false
+        logallrefupdates = true
+        ignorecase = true
+        precomposeunicode = true
+[remote "origin"]
+        url = https://gaohongwei@github.com/gaohongwei/rails_topics.git
+        fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "master"]
+        remote = origin
+        merge = refs/heads/master
+[user]
+        email = gaohongwei@gmail.com
+        name = gaohong wei
+        username = abc
