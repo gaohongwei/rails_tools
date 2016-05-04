745a6a7c7166ce1fad148f27c30f8c67e38b1ffe
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2016-05-03 21:54:11 -0700

    Update db_table_change.rb

--- db_dm/db_table_change.rb
+++ db_dm/db_table_change.rb
@@ -1,3 +1,9 @@
 ##### Update table definition
-rails g migration RemoveCommantableIdFromComment commantable_id:integer
+
+
+### Add column
 rails g migration AddCommentableIdToComment      commentable_id:integer
+rails g migration AddTypeToUser type:string
+
+### Remove columns
+rails g migration RemoveCommantableIdFromComment commantable_id:integer
