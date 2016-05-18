6e105eddd01b8ca52837a71dc0d053ebd0ef2a9a
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2016-05-17 21:02:23 -0700

    Create hide_show.js

---
+++ ui/hide_show.js
@@ -0,0 +1,12 @@
+
+  $('tr td').hover(show_hide('show'),show_hide('hide'));
+  function show_hide(act){
+    return function(){
+      var dom=$(this).find('.show_or_hide')
+      if ('show' == act){
+        dom.removeClass('hide_me');
+      } else {
+        dom.addClass('hide_me');
+      }
+    }
+  }
