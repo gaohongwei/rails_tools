2aa12cbaf4277d2c619990b8acd4ecae4a075fbb
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2016-05-24 16:30:20 -0700

    Update git_command.rb

--- git/git_command.rb
+++ git/git_command.rb
@@ -1,8 +1,15 @@
 
+ssh-keygen -t rsa
+ssh-copy-id remote_host
+ssh -p port_number remote_host
+ssh remote_host command_to_run
+
 Testing your SSH connection
 ssh -T git@github.com
 
-git push origin master --username
+git push origin master --usernamessh -p port_number remote_host
+ssh remote_host command_to_run
+
 git config --global user.email
 
 git clone https://gaohongwei@github.com/gaohongwei/rails_topics
