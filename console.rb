04fe0675803dfc3e785569c067cf1a8fa3bbedcb
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2016-07-08 11:27:11 -0700

    Update console.rb

--- console.rb
+++ console.rb
@@ -6,3 +6,7 @@ Print a Rails configuration in console
 Rails.application.config.action_mailer
 Rails.application.config.<your_variable>
 Rails.application.config.instance_variables
+
+=============Mongoid Config
+Mongoid::Config.sessions => {"default"=>{"database"=>"aperture_development", "hosts"=>["localhost:27017"], "options"=>nil}}
+Mongoid::Config.methods
