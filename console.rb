c7e72a4474b2782dc2b76dcb35e32421dd9c9aa4
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2016-07-08 10:25:32 -0700

    Update console.rb

--- console.rb
+++ console.rb
@@ -5,3 +5,4 @@ include ApplicationHelper
 Print a Rails configuration in console
 Rails.application.config.action_mailer
 Rails.application.config.<your_variable>
+Rails.application.config.instance_variables
