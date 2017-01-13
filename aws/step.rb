e6f40abb8a7e7ef1570def48bd4175ec1cd24b31
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-01-12 19:15:55 -0800

    Create step.rb

---
+++ aws/step.rb
@@ -0,0 +1,31 @@
+Initializer: aws.rb
+AWS.config(
+  access_key_id: '',
+  secret_access_key: '',
+  region: 'region'
+)
+
+Initializer: paperclip.rb
+Paperclip.interpolates('cs_user_md5') do |attachment, _style|
+  "#{Digest::MD5.hexdigest(attachment.instance.cs_user)}"
+end
+
+development.rb
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: 'bucket_name'
+    },
+    path: '/:cs_user_md5/:class/:id.:extension',
+    url: '/:cs_user_md5/:class/:id/:filename'
+  }
+
+Model
+  has_mongoid_attached_file :attachment,
+    :path           => '/:cs_user_md5/attachment/:id/:style.:extension',
+    :storage        => :s3,
+    #:s3_host_alias  => 'something.cloudfront.net',
+    #:s3_credentials => File.join(Rails.root, 'config', 's3.yml'),
+    s3_credentials: {
+      bucket: 'bucket_name'
+    }
