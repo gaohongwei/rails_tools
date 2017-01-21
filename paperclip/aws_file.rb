485ae85c55ac301ec6177eeff3068da18a9ccf8b
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-01-20 16:12:37 -0800

    Create aws_file.rb

---
+++ paperclip/aws_file.rb
@@ -0,0 +1,33 @@
+def image
+  begin
+    image = S3Store.new(params[:upload][:image]).store
+    #...
+  rescue Exception => e
+    #...
+  end
+end
+
+class S3Store
+  BUCKET = "app-uploads".freeze
+
+  def initialize file
+    @file = file
+    @s3 = AWS::S3.new
+    @bucket = @s3.buckets[BUCKET]
+  end
+
+  def store
+    @obj = @bucket.objects[filename].write(@file.tempfile, acl: :public_read)
+    self
+  end
+
+  def url
+    @obj.public_url.to_s
+  end
+
+  private
+  
+  def filename
+    @filename ||= @file.original_filename.gsub(/[^a-zA-Z0-9_\.]/, '_')
+  end
+end
