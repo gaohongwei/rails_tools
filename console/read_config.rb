f6668ac453d5971ecb015551aa9c0693fd032de9
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-02-02 12:07:28 -0800

    Update read_config.rb

--- console/read_config.rb
+++ console/read_config.rb
@@ -10,3 +10,11 @@ Rails.application.config.instance_variables
 =============Mongoid Config
 Mongoid::Config.sessions => {"default"=>{"database"=>"aperture_development", "hosts"=>["localhost:27017"], "options"=>nil}}
 Mongoid::Config.methods
+
+
+=============Current Directory
+Dir.pwd
+
+=============Load/require
+require_relative requires a file specifically pointed to relative to the file that calls it.
+require requires a file included in the $LOAD_PATH.
