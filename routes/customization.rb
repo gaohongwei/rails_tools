a35a46d8a0e1b59ac27d500b46f711bde1348456
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-02-07 15:41:42 -0800

    Update customization.rb

--- routes/customization.rb
+++ routes/customization.rb
@@ -2,24 +2,24 @@
 A member route will require an ID, because it acts on a member. 
 A collection route doesn't because it acts on a collection of objects. 
 
-
   resources :syslogs, only: :index do
     collection do
-      post :add_receiver
-      post :add_client
+      post :add_rcv
+      post :add_app
     end
     member do
-      delete :delete_receiver
+      delete :delete_rcv
       delete :delete_app
     end
   end
 
-td = link_to "Delete ", delete_receiver_syslog_path(receiver['id']), method: :delete, data: {confirm: "You sure?"}
+td = link_to "Delete ", delete_rcv_syslog_path(rcv['id']), method: :delete, data: {confirm: "You sure?"}
 td = link_to "Delete ", delete_app_syslog_path(app['id']), method: :delete, data: {confirm: "You sure?"}
 
-  def delete_receiver
-    redirect_to syslogs_path
+  def delete_rcv
+    redircvt_to syslogs_path
   end
+
   def delete_app
-    redirect_to syslogs_path
+    redircvt_to syslogs_path
   end
