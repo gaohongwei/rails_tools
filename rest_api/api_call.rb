d6c4117e5879d38e22d280b72c7cfe8beee2e41f
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-02-17 15:48:22 -0800

    Update api_call.rb

--- rest_api/api_call.rb
+++ rest_api/api_call.rb
@@ -1,15 +1,16 @@
 #### API call
-class ApiService < ApiBase
+class ApiService
+  include ApiBase
   def add(data)
-    client.request(:post, url, app_params(data).to_json, com_header)
+    self.class.request(:post, url, app_params(data).to_json, com_header)
   end
 
   def list(page, size)
-    client.request(:get, list_url, { page: page, size: size }, com_header)
+    self.class.request(:get, list_url, { page: page, size: size }, com_header)
   end
 
   def delete(id)
-    client.request(:delete, url(id), {}, com_header)
+    self.class.request(:delete, url(id), {}, com_header)
   end
 #### API parse
   read:200, create:201, delete: 204
