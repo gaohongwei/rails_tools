8d3898f162ac4beb2a82dc5b60e0d336f2118a14
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-02-17 15:49:01 -0800

    Update api_base.rb

--- rest_api/api_base.rb
+++ rest_api/api_base.rb
@@ -1,45 +1,48 @@
 require 'logger'
 
 module ApiBase
-  def self.request(method, url, payload, header = headers)
-    execute(2) do
-      connection.public_send(method, url, payload, header)
+  extend ActiveSupport::Concern
+  class_methods do
+    def request(method, url, payload, header = headers)
+      execute(2) do
+        connection.public_send(method, url, payload, header)
+      end
     end
-  end
 
-  def self.connection
-    Faraday.new do |conn|
-      conn.use CustomErrors # 
-      conn.use Faraday::Adapter::NetHttp
-      conn.request :url_encoded
-      conn.options[:open_timeout] = 2
-      conn.options[:timeout] = 5
+    def connection
+      Faraday.new do |conn|
+        conn.use CustomErrors
+        conn.use Faraday::Adapter::NetHttp
+        conn.request :url_encoded
+        conn.options[:open_timeout] = 2
+        conn.options[:timeout] = 5
+      end
     end
-  end
 
-  def self.execute(retries = 5, &block)
-    return block.call
-  rescue RuntimeError => e # if error 500
-    if retries > 0
-      retries -= 1
-      sleep 1
-      retry
+    def execute(retries = 5, &block)
+      return block.call
+    rescue RuntimeError => e # if error 500
+      if retries > 0
+        retries -= 1
+        sleep 1
+        retry
+      end
+      return logger_error(e)
+    rescue Faraday::Error => e # if webservice not accessible
+      return logger_error(e)
     end
-    return logger_error(e)
-  rescue Faraday::Error => e # if webservice not accessible
-    return logger_error(e)
-  end
 
-  def self.headers
-    {
-      'Content-Type' => 'application/json',
-      'Accept' => 'application/json'
-    }
-  end
+    def headers
+      {
+        'Content-Type' => 'application/json',
+        'Accept' => 'application/json'
+      }
+    end
 
-  def self.logger_error(e)
-    error_header = 'ERROR - ServiceNotFound'
-    Rails.logger.error "#{error_header} : #{e}"
-    OpenStruct.new(status: 500, details: "#{e}")
+    def logger_error(e)
+      error_header = 'ERROR - ServiceNotFound'
+      Rails.logger.error "#{error_header} : #{e}"
+      OpenStruct.new(status: 500, details: "#{e}")
+    end
   end
 end
