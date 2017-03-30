76f9e431df04a1c69b75582790ee1133bd6924b9
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-03-29 17:16:05 -0700

    Create step0_options.rb

---
+++ oauth2/step0_options.rb
@@ -0,0 +1,19 @@
+=> {  
+  "setup"=>false,
+ "skip_info"=>false,
+ "client_id"=>"client_id",
+ "client_secret"=>"client_secret",
+ "client_options"=>
+    { 
+      "site" => "abc.com",
+      "authorize_url"=>"https://secure.sharefile.com/oauth/authorize",
+      "token_url"=>"https://secure.sharefile.com/oauth/token"
+     },
+ "authorize_params"=>{},
+ "authorize_options"=>[:scope],
+ "token_params"=>{},
+ "token_options"=>[],
+ "auth_token_params"=>{},
+ "provider_ignores_state"=>false,
+ "name"=>"citrix"
+}
