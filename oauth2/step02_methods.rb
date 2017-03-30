b6d6a4ba75fe680225b62b10e0ed7f97c6dfbf1c
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-03-30 13:28:20 -0700

    Update step02_methods.rb

--- oauth2/step02_methods.rb
+++ oauth2/step02_methods.rb
@@ -13,21 +13,9 @@ options = {
   'nested' => { 'nested_a' => 'nested_a_value'}
 }
 h = deep_symbolize(options)
-
-options = {
+h has this value
+{
   authorize_url: "authorize_url_value",
   token_url: "token_url_value",
   nested: { nested_a: 'nested_a_value'}
 }
-########## options_for ##########
-# options_for("authorize")
-# authorize_options = [:read, :scope]
-# read keys from authorize_options
-# select the key if options[:scope] or  options[:read]
-def options_for(option)
-  hash = {}
-  options.send(:"#{option}_options").select { |key| options[key] }.each do |key|
-    hash[key.to_sym] = options[key]
-  end
-  hash
-end
