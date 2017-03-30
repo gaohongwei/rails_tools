cd8170f515df2cf44e5106d2aa4b6186e719302d
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-03-29 20:05:17 -0700

    Update step0_options.rb

--- oauth2/step0_options.rb
+++ oauth2/step0_options.rb
@@ -73,6 +73,18 @@ initialize/omniauth.rb
       param_name: 'token'
     }
 
+    def option(name, value = nil)
+      default_options[name] = value
+    end
+
+    def default_options
+      return @default_options if instance_variable_defined?(:@default_options) && @default_options
+      existing = superclass.respond_to?(:default_options) ? superclass.default_options : {}
+      @default_options = OmniAuth::Strategy::Options.new(existing)
+    end
+
+    @options = self.class.default_options.dup
+
   def options_for(option)# OAuth2
     hash = {}
     options.send(:"#{option}_options").select { |key| options[key] }.each do |key|
