a00e284d28889726b3a82d2f4268e32909dc9550
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-05-04 09:55:20 -0700

    Update note.rb

--- config_deploy/note.rb
+++ config_deploy/note.rb
@@ -13,7 +13,7 @@ mkdir -p lib/capistrano/tasks
 create Capfile
 Capified
 
-##### 2. update vaibales  #####
+##### 2. Define variables  #####
 domain = "mydomain"
 home = "/home/myuser"
 
@@ -25,6 +25,9 @@ Alternatively, you can use fetch, the counterpart to set:
 set :domain, "mydomain"
 set :home, "/home/myuser"
 set :deploy_to, "#{fetch(:home)}/#{fetch(:domain)}"
+
+fetch can define a default variable
+set :deploy_to, "#{fetch(:home)}/#{fetch(:domain, 'localhost')}"
 
 ##### 3. Create tasks  #####
 similar to rake task
@@ -46,3 +49,20 @@ namespace :deploy do
 cap -T
 cap dev deploy:get_code
 config/deploy/dev.rb must be defined
+
+##### 4. Run command remotely  #####
+Need to define
+server, user, role
+run a command, 
+run "ls"
+run "#{sudo} cp ~/hello /hello"
+
+set :user, "deployer"
+server '127.0.0.1', :web, :app, :db, :sms, :cache, primary: true
+primary: true is used for db
+
+
+role :web, '10.10.10.10', '10.10.10.11' # if many servers
+
+##### 5. source code  #####
+lib/capistrano/recipes
