64fe65c9591616df10ce0fe8dee87d6bb55df4db
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-05-04 14:42:32 -0700

    Update cap_tasks.rb

--- config_deploy/cap_tasks.rb
+++ config_deploy/cap_tasks.rb
@@ -1,15 +1,35 @@
+#  cap tasks are similar to rake tasks
+#  http://railscasts.com/episodes/133-capistrano-tasks
+set :application, "railscasts.com"
+role :app, application
+role :web, application
+role :db,  application, :primary => true
 
-##### 3. Create tasks  #####
-similar to rake task
+set :user, "deploy"
+set :deploy_to, "/var/www/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository, "git://github.com/ryanb/railscasts.git"
+set :branch, "master"
+
 namespace :deploy do
-  desc 'Hello get_code'
-  task :get_code do
-    puts 'get_code'
+  desc "Tell Passenger to restart the app."
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
   end
-
-  desc 'Hello build_asset'
-  task :build_asset do
-    puts 'build_asset'
+  
+  desc "Symlink shared configs and folders on each release."
+  task :symlink_shared do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
   end
+  
+  desc "Sync the public/assets directory."
+  task :assets do
+    system "rsync -vr --exclude='.DS_Store' public/assets #{user}@#{application}:#{shared_path}/"
+  end
+end
 
-  after :get_code, :build_asset
+after 'deploy:update_code', 'deploy:symlink_shared'
