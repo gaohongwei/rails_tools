9c4833b3af1427ed060d34ce6952db84a16169f8
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-05-13 16:23:42 -0700

    Update git_command.rb

--- git/git_command.rb
+++ git/git_command.rb
@@ -1,3 +1,17 @@
+# Set user and email for single  project
+git init
+git config user.name "Gaohong Wei"
+git config user.name
+git config user.email "gaohongwei@gmail.com"
+git config user.email
+
+# Set user and email globally
+git init
+git config --global user.name "Gaohong Wei"
+git config --global user.name
+git config --global user.email "gaohongwei@gmail.com"
+git config --global user.email
+
 
 ssh-keygen -t rsa
 ssh-copy-id remote_host
@@ -10,7 +24,6 @@ ssh -T git@github.com
 git push origin master --usernamessh -p port_number remote_host
 ssh remote_host command_to_run
 
-git config --global user.email
 
 git clone https://gaohongwei@github.com/gaohongwei/rails_topics
 
