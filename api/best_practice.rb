105dbb3edff81430057feefadadd58f50ff0a3c7
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-05-20 15:56:18 -0700

    Update best_practice.rb

--- api/best_practice.rb
+++ api/best_practice.rb
@@ -43,24 +43,10 @@ We are using the url for the API versioning starting with the letter „v“
 It is hard to work with an API that ignores error handling. Pure returning of a HTTP 500 with a stacktrace is not very helpful.
 Use HTTP status codes
 
-The HTTP standard provides over 70 status codes to describe the return values. We don’t need them all, but  there should be used at least a mount of 10.
-
-200 – OK – Eyerything is working
-201 – OK – New resource has been created
-204 – OK – The resource was successfully deleted
-
-304 – Not Modified – The client can use cached data
-
-400 – Bad Request – The request was invalid or cannot be served. The exact error should be explained in the error payload. E.g. „The JSON is not valid“
-401 – Unauthorized – The request requires an user authentication
-403 – Forbidden – The server understood the request, but is refusing it or the access is not allowed.
-404 – Not found – There is no resource behind the URI.
-422 – Unprocessable Entity – Should be used if the server cannot process the enitity, e.g. if an image cannot be formatted or mandatory fields are missing in the payload.
-
-500 – Internal Server Error – API developers should avoid this error. If an error occurs in the global catch blog, the stracktrace should be logged and not returned as response.
+The HTTP standard provides over 70 status codes to describe the return values. 
+We don’t need them all, but  there should be used at least a mount of 10.
 
 Use error payloads
-
 All exceptions should be mapped in an error payload. Here is an example how a JSON payload should look like.
  
 10. Allow overriding HTTP method
